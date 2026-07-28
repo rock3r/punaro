4d09ca0a9e6396e9e8c1f349a7167844f47168e4
Sebastiano Poggi <poggos@gmail.com>
2026-07-28 08:56:36 +0200

    Limit released consolidation history

--- internal/postgres/brain_consolidation_schema.go
+++ internal/postgres/brain_consolidation_schema.go
@@ -96,7 +96,7 @@ FROM objects,routine_safety,constraint_safety,table_acl_safety`, claimMD5, advan
 const (
 	memoryConsolidationClaimRoutineMD5      = "121df7d09493be8662f4618208aaf342"
 	memoryConsolidationAdvanceRoutineMD5    = "5666d576e054c6b06999a0b6ce7b6c62"
-	memoryConsolidationSourcesRoutineMD5    = "ef38591b88d4ed599156e055831d8c2f"
+	memoryConsolidationSourcesRoutineMD5    = "2b180c012d8c1ae7332b81456845a8bf"
 	memoryConsolidationV33ClaimRoutineMD5   = "32e95c7fb6a9e73522c73b825bc3dcea"
 	memoryConsolidationV33AdvanceRoutineMD5 = "cce038c3cca8f3c4f48da8b5e155443c"
 )
