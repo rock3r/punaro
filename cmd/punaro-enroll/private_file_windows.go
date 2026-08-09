0e714ca858a104cf22828f6166db6e86959dbea1
Sebastiano Poggi <poggos@gmail.com>
2026-08-09 11:50:37 +0200

    fix Windows enrollment publication race

--- cmd/punaro-enroll/private_file_windows.go
+++ cmd/punaro-enroll/private_file_windows.go
@@ -190,7 +190,48 @@ func writePrivateAtomic(path string, raw []byte) error {
 	return nil
 }
 
-func writePrivateAtomicNew(path string, raw []byte) error { return writePrivateAtomic(path, raw) }
+func writePrivateAtomicNew(path string, raw []byte) error {
+	file, err := os.CreateTemp(filepath.Dir(path), "."+filepath.Base(path)+".tmp-*")
+	if err != nil {
+		return err
+	}
+	temporary := file.Name()
+	removeTemporary := true
+	defer func() {
+		if removeTemporary {
+			_ = os.Remove(temporary)
+		}
+	}()
+	if err := file.Close(); err != nil {
+		return err
+	}
+	if err := protectWindowsPath(temporary); err != nil || !privateWindowsACL(temporary) {
+		return errors.New("could not protect private file")
+	}
+	file, err = os.OpenFile(temporary, os.O_WRONLY|os.O_TRUNC, 0) // #nosec G304 -- DACL is verified private before this secret write.
+	if err != nil {
+		return err
+	}
+	if _, err := file.Write(raw); err != nil {
+		_ = file.Close()
+		return err
+	}
+	if err := file.Sync(); err != nil {
+		_ = file.Close()
+		return err
+	}
+	if err := file.Close(); err != nil {
+		return err
+	}
+	if err := os.Link(temporary, path); err != nil {
+		return err
+	}
+	if err := os.Remove(temporary); err != nil {
+		return err
+	}
+	removeTemporary = false
+	return nil
+}
 func writeCredential(path, credential string) error {
 	if credential == "" || strings.ContainsAny(credential, " \t\r\n") {
 		return errors.New("invalid credential")
